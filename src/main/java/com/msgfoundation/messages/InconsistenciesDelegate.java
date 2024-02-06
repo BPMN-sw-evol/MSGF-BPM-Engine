157533aa698c621e57f6bebac4d6a97a4506bfd1
jhonalexander019 <jhon.roa@unillanos.edu.co>
2024-02-05 21:57:35 -0500

    updating own annotations

--- src/main/java/com/msgfoundation/messages/InconsistenciesDelegate.java
+++ src/main/java/com/msgfoundation/messages/InconsistenciesDelegate.java
@@ -1,14 +1,26 @@
 package com.msgfoundation.messages;
 
+import com.msgfoundation.annotations.BPMNGetterVariables;
+import com.msgfoundation.annotations.BPMNTask;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import java.time.LocalDateTime;
 
+@BPMNTask(type="Send Task", name="Informar inconsistencias")
 public class InconsistenciesDelegate implements JavaDelegate {
     @Override
+    @BPMNGetterVariables(variables = {"coupleName1", "coupleName2", "creationDate"})
     public void execute(DelegateExecution delegateExecution) throws Exception {
+
+        String coupleName1 = (String) delegateExecution.getProcessInstance().getVariables().get("coupleName1");
+        String coupleName2 = (String) delegateExecution.getProcessInstance().getVariables().get("coupleName2");
+        String creationDate = (String) delegateExecution.getProcessInstance().getVariables().get("creationDate");
+        String codInstance = (String) delegateExecution.getProcessInstanceId();
+
         System.out.println("INFORME DE INCONSISTENCIAS EN SOLICITUD\n" +
-                "La solicitud de crédito ha sido rechazada por inconsistencias en los requisitos\n" +
-                "Código de solicitud: "+delegateExecution.getProcessInstanceId() +
-                "\nFecha de rechazo: "+"Hoy :D");
+                "Estimados " + coupleName1 + " y " + coupleName2 + ", la solicitud de crédito realiazada el dia " +creationDate + "ha sido rechazada por inconsistencias en los requisitos.\n" +
+                "Código de solicitud: "+ codInstance +
+                "\nFecha de rechazo: "+ LocalDateTime.now());
+
     }
 }
--- src/main/java/com/msgfoundation/messages/RequestRejectByCommitteeDelegate.java
+++ src/main/java/com/msgfoundation/messages/RequestRejectByCommitteeDelegate.java
@@ -1,14 +1,30 @@
 package com.msgfoundation.messages;
 
+import com.msgfoundation.annotations.BPMNGetterVariables;
+import com.msgfoundation.annotations.BPMNTask;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import java.time.LocalDateTime;
 
+
+import java.time.LocalDateTime;
+
+@BPMNTask(type="Send Task", name="Informar rechazo de solicitud")
 public class RequestRejectByCommitteeDelegate implements JavaDelegate {
     @Override
+    @BPMNGetterVariables(variables = {"coupleName1", "coupleName2", "housePrice", "coupleSaving"})
     public void execute(DelegateExecution delegateExecution) throws Exception {
+
+        String coupleName1 = (String) delegateExecution.getProcessInstance().getVariables().get("coupleName1");
+        String coupleName2 = (String) delegateExecution.getProcessInstance().getVariables().get("coupleName2");
+        Long housePrice = (Long) delegateExecution.getProcessInstance().getVariables().get("housePrice");
+        Long coupleSaving = (Long) delegateExecution.getProcessInstance().getVariables().get("coupleSavings");
+        String codInstance = (String) delegateExecution.getProcessInstanceId();
+
         System.out.println("INFORME DE RECHAZO POR PARTE DEL COMITÉ DE CRÉDITO\n" +
-                "La solicitud de crédito ha sido es rechazada por concepto del comité\n" +
-                "Código de solicitud: "+delegateExecution.getProcessInstanceId() +
-                "\nFecha de rechazo: "+"Hoy :D");
+                "Estimados " + coupleName1 + " y " + coupleName2 + ", la solicitud de crédito ha sido es rechazada por concepto del comité,\n" +
+                "el costo de la vivienda (" + housePrice + ") es demasido alto y los ahorros registrados (" + coupleSaving + ") no cumplen con la normatividad. \n"+
+                "Código de solicitud: "+ codInstance +
+                "\nFecha de rechazo: "+ LocalDateTime.now());
     }
 }
